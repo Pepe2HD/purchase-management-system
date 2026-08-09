4e95389bda31ca14363e00d79fecebc0a1bc3ade
Pepe2HD <pedrohd977@gmail.com>
2026-08-09 14:38:17 -0300

    feat: add resource routes for purchase requests

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PurchaseRequestController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('purchase_requests', PurchaseRequestController::class);
